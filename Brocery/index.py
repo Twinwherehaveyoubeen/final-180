68c1f0c09cf0c3c537c17cec28bab65504ac4af5
Jose <jjmaximo13@gmail.com>
2025-04-16 10:02:25 -0400

    made the log in and the sign up

--- Brocery/index.py
+++ Brocery/index.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import mysql.connector
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -19,6 +20,22 @@ def home():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+# @app.route ('/signup', methods={'GET', 'POST'})
+# def signup():
+#     if request.method =='POST':
+#         username = request.form['username']
+#         email = request.form['email']
+#         account_type = request.form['account_type']
+#         password = generate_password_hash(request.form{'password'})
+
+#         id existing:
+#             flash("User already exist ")
+#         return redirect
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
